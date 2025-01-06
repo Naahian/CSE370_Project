f472085d04124c8c7a1fe07be8afd52e8d2e352e
AhnafAwsaf7 <140892494+AhnafAwsaf7@users.noreply.github.com>
2025-01-06 22:49:43 +0600

    Add files via upload

--- CSE370_Project/asgi.py
+++ CSE370_Project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CSE370_Project project.
+ASGI config for demo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CSE370_Project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
 
 application = get_asgi_application()
