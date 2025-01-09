403126dd489d6f09415fa621e93a95e570920f0c
AhnafAwsaf7 <140892494+AhnafAwsaf7@users.noreply.github.com>
2025-01-09 17:04:25 +0600

    Update views.py

--- profiles/views.py
+++ profiles/views.py
@@ -8,6 +8,7 @@ def profile_view(request:HttpRequest):
     if(not request.user.is_authenticated):
         return redirect('/users/login')
     profile = Profile.objects.get(user=request.user)
+    #SELECT * FROM profile WHERE user_id = <user_id>;
     return render(request, 'profile_view.html', {'profile': profile})
 
 
